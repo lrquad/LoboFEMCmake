9ec672e1da6402c63c088535aff9ffd126db1ac4
RanLuo <lrkk1234@gmail.com>
2019-08-29 00:02:00 -0600

    udpate py

--- scripts/convertVideoGif.py
+++ scripts/convertVideoGif.py
@@ -15,6 +15,6 @@ if __name__ == "__main__":
     
     bashCommand = "ffmpeg -i " + config.videopath+" -vf fps=15,scale=640:-1:flags=lanczos,palettegen " + config.videopath+".png"
     os.system(bashCommand)
-    bashCommand = "ffmpeg -t" + config.t +" -ss " +config.ss+ " -i " + config.videopath+ " -i " + config.videopath+".png" + " -filter_complex \"fps=15,scale=640:-1:flags=lanczos[x];[x][1:v]paletteuse\" " + config.gifpath
+    bashCommand = "ffmpeg -t " + config.t +" -ss " +config.ss+ " -i " + config.videopath+ " -i " + config.videopath+".png" + " -filter_complex \"fps=15,scale=640:-1:flags=lanczos[x];[x][1:v]paletteuse\" " + config.gifpath
     os.system(bashCommand)
 
