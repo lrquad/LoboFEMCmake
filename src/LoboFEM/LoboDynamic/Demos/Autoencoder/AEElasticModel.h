989e1cceef69f0e3926a85243d4dd6414ab5fa2c
ranluo <lrkk1234@gmail.com>
2019-09-12 17:01:59 -0600

    update AE model

--- src/LoboFEM/LoboDynamic/Demos/Autoencoder/AEElasticModel.h
+++ src/LoboFEM/LoboDynamic/Demos/Autoencoder/AEElasticModel.h
@@ -1,9 +1,19 @@
 #include "LoboDynamic/LoboDynamic.h"
 
+template <class TYPE>
+class AEAutoDiffModel;
+
 namespace Lobo
 {
-    class AEElasticModel:public DynamicModel
-    {
-        
-    }
+
+class AEElasticModel : public DynamicModel
+{
+public:
+    AEElasticModel(LoboDynamicScene *scene_, LoboTetMesh *tetmesh_, HyperelasticModel *elastic_model_, AEAutoDiffModel<double> *ae_ad_model_);
+    ~AEElasticModel();
+
+    LoboTetMesh *tetmesh;
+    HyperelasticModel *elastic_model;
+    AEAutoDiffModel<double> *ae_ad_model;
 }
+} // namespace Lobo
--- src/LoboFEM/LoboDynamic/Demos/Autoencoder/DemoDinosaurAE.h
+++ src/LoboFEM/LoboDynamic/Demos/Autoencoder/DemoDinosaurAE.h
@@ -1,6 +1,34 @@
 #pragma once
+#include "LoboDynamic/LoboDynamicSolver/DynamicSolver.h"
 
 namespace Lobo
 {
-    
-}
+
+class AEElasticModel;
+class HyperelasticModel;
+class LoboTimeIntegration;
+class LoboTetMesh;
+class AEKineticModel;
+
+class DemoDinosaurAE : public DynamicSolver
+{
+public:
+    DemoDinosaurAE(Lobo::LoboDynamicScene *parent_scene);
+    ~DemoDinosaurAE();
+
+    virtual void drawImGui();
+
+    virtual void runXMLscript(pugi::xml_node &solver_node);
+
+    virtual void precompute();
+    virtual void stepForward();
+    virtual int getCurStep();
+
+    ConstrainModel *constrainmodel;
+    CollisionModel *collisionmodel;
+    HyperelasticModel *hyperelastic_model;
+    AEKineticModel *kinetic_model;
+    LoboTimeIntegration *time_integraion;
+};
+
+} // namespace Lobo
